d921aec57c5486c01c6f53fdd5c229098ee16457
hsichang <hsi.chang@gmail.com>
2014-05-08 23:05:45 -0400

    more work on the js

--- js/checkers.js
+++ js/checkers.js
@@ -1,21 +1,15 @@
 $(document).ready(function() {
   var Checkers = function() {
-    this.$variable = $('#game');
-
-    /* page states */
-    this.$frontPageContainer = $('#main');
-    this.$boardContainer = $('#board-container');
+    this.$body = $('body');
 
     /* board */
     this.$board = $('#board');
-    this.$squares = $('.square');
 
     /* pieces */
     this.$red = $('.red');
     this.$green = $('.green');
     this.$brown = $('.brown');
     this.$orange = $('.orange');
-    /* todo ==> add the king images */
 
     /* debug divs */
     this.$debugSquareDisplay = $('#debug-display-square');
@@ -44,15 +38,11 @@ $(document).ready(function() {
           testing = true;
 
       if (testing) {
-        self._toggleBoard("gameboard");
+        self._toggleBoard("playing");
         self.$board.mousemove(function(evt) {
           self._debug(evt, "display-square");
         });
       };
-
-      $(document).click(function(evt) {
-        self._targetClickListener(evt);
-      });
 
       self._buildChatRoom();
       /* on keypress maybe inside chat */
@@ -66,93 +56,95 @@ $(document).ready(function() {
       var self = this,
           target = evt.target.id,
           $targetDiv = $('#' + target);
-
+/*
       if (squares[target].legal && action === "display-square") {
         self.$debugSquareDisplay.text("#"+target);
       };
-    },
-
-    _targetClickListener: function(evt) {
-      var self = this,
-          target = evt.target.id,
-          $targetDiv = $('#' + target);
-
-      if (squares[target].legal) {
-        $targetDiv.toggleClass('highlight');
-      };
+      */
     },
 
     _toggleBoard: function(state) {
-      /* TODO: make this evt based
-       *
-       * if the evt is a start game -> create board
-       * if the evt is a win game -> show leaderboard
-       *
-       */
-
       var self = this;
-
-      if ( state === "gameboard") {
-        self.$frontPageContainer.toggleClass('inactive');
-        self.$boardContainer.toggleClass('active');
-        squares = self._buildGameBoard(2); /* 2 or 4 players */
-      };
+      if (state === "playing") { self._buildGameBoard() };
     },
 
-    _buildGameBoard: function(players) {
+    _buildGameBoard: function() {
       var self = this,
-          rows, cols, square, legal_space, squares;
+          rows, cols, squares, legal_space, squares;
 
       squares = {};
-      if (players === 2) {
-        rows = 8;
-        cols = 8;
+      rows = 8;
+      cols = 8;
+      legal = false;
 
-        legal = false;
-        for (r=1; r<rows+1; r++) {
-          self.$board.append('<div id="r' + r + '" class="row"></div>');
-          for (c=1; c<cols+1; c++) {
-            legal_space = legal ? "legal" : "illegal";
-            square_name = "r" + r + "c" + c;
-            square = '<div id="' + square_name+'" class="square ' + legal_space + '"></div>';
-            $('#r'+r).append(square);
+      self.$body.toggleClass('playing');
 
-            squares[square_name] = new Square(legal, r, c);
-            if (squares[square_name].player === 1) { self._positionPiece(square_name, 1) };
-            if (squares[square_name].player === 2) { self._positionPiece(square_name, 2) };
-            legal = !legal
+      for (r=1; r<rows+1; r++) {
+        self.$board.append('<div id="r' + r + '" class="row"></div>');
+        for (c=1; c<cols+1; c++) {
+          legal_space = legal ? "legal" : "illegal";
+          square_name = "c" + c + "r" + r;
+          square = '<div id="' + square_name+'" class="square ' + legal_space + '"></div>';
+          $('#r'+r).append(square);
 
-        /* todo:
-         * maybe build a hash table here
-         * the hash table could have an array
-         * of info about the square
-         * does it have a legal move?
-         * does it have a chip
-         * can it be activated?
-         * etc
-         *
-         * i.e.,
-         * Hash[:space(/r2c4/)][:legal] => true
-         *                     [:chip] => green
-         *                     [:chip] => null
-         *                     [:legal] => [r3c3,r3c3]
-         *                     [:etc] => etc;
-         */
-
-          };
+          squares[square_name] = new Square(legal, r, c);
+          if (squares[square_name].player === 1) { self._positionPiece(square_name, 1) };
+          if (squares[square_name].player === 2) { self._positionPiece(square_name, 2) };
           legal = !legal
+
+      /* todo:
+       * maybe build a hash table here
+       * the hash table could have an array
+       * of info about the square
+       * does it have a legal move?
+       * does it have a chip
+       * can it be activated?
+       * etc
+       *
+       * i.e.,
+       * Hash[:space(/r2c4/)][:legal] => true
+       *                     [:chip] => green
+       *                     [:chip] => null
+       *                     [:legal] => [r3c3,r3c3]
+       *                     [:etc] => etc;
+       */
+
         };
-
-        return squares;
+        legal = !legal
       };
+
+      self._move(squares, 1);
     },
 
-    _positionPiece: function(square, player) {
-      $('#'+square).toggleClass('occupied');
-      $('#'+square).toggleClass('piece_player_'+player);
+    _positionPiece : function(square, player) {
+      $('#'+square).toggleClass('occupied player_'+player);
     },
 
-    _buildChatRoom: function() {
+    _move : function(squares, player) {
+      var self = this;
+      var $squares = $('.square');
+
+      $('#move-banner').text('Player ' + player);
+
+      $squares.on("click", function(evt) {
+        self._evaluateMoves(squares, player, evt.currentTarget.id);
+      });
+    },
+
+    _evaluateMoves : function(squares, player, square_id) {
+/* TODO: up to here - basically make this and the "_move" function into one, there is a lot of redundancy here.  then _evaluateMoves.  should be a functoin with only one parameter, the square you are checking.  the recursion of it goes above in the "move" */
+      var moves = {};
+      var $target = $('#'+square_id);
+      if (player === 1) { direction = 1 } else { direction = -1 };
+      if ($target.hasClass("occupied player_"+player)) {
+        $target.toggleClass('highlight');
+      };
+      next = "c" + (squares[square_id].column + direction) + "r" + (squares[square_id].row + 1)
+      next = "c" + (squares[square_id].column + direction) + "r" + (squares[square_id].row - 1)
+      self._evaluateNextMove(
+    },
+
+    _buildChatRoom : function() {
       console.log("...building chat room");
 
     },
